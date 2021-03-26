c4ac40a5028da7501f2eb9908835b018b46489bd
Mariya <mariyasin@list.ru>
2021-03-27 00:32:31 +0300

    updated model response

---
+++ src/NewsService.Models.Dto/ModelResponse/NewsResponse.cs
@@ -0,0 +1,20 @@
+﻿using LT.DigitalOffice.NewsService.Models.Dto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.NewsService.Models.Dto.ModelResponse
+{
+    public class NewsResponse
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public string Subject { get; set; }
+        public User Author { get; set; }
+        public User Sender { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Guid DepartamentId { get; set; }
+    }
+}
