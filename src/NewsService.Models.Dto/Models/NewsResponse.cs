262a4e51333ec37a31cc9cc6adac901726429a85
Mariya <mariyasin@list.ru>
2021-03-26 22:04:10 +0300

    added response model

---
+++ src/NewsService.Models.Dto/Models/NewsResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.NewsService.Models.Dto.Models
+{
+    public class NewsResponse
+    {
+        public string Content { get; set; }
+        public string Subject { get; set; }
+        public string AuthorName { get; set; }
+        public Guid AuthorId { get; set; } // Сheck with Spartack
+        public Guid SenderId { get; set; } // Сheck with Spartack
+        public DateTime CreatedAt { get; set; } // Сheck with Spartack
+        public Guid DepartamentId { get; set; } // Сheck with Spartack
+        public bool IsActive { get; set; } // Сheck with Spartack
+    }
+}
