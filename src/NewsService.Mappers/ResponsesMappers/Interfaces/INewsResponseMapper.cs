ed8222d44996456f2074935bafbbf2af6d05093a
Mariya <mariyasin@list.ru>
2021-03-31 03:58:02 +0300

    added logger to maper

--- src/NewsService.Mappers/ResponsesMappers/Interfaces/INewsResponseMapper.cs
+++ src/NewsService.Mappers/ResponsesMappers/Interfaces/INewsResponseMapper.cs
@@ -1,6 +1,7 @@
 ﻿using LT.DigitalOffice.NewsService.Mappers.Interfaces;
 using LT.DigitalOffice.NewsService.Models.Db;
 using LT.DigitalOffice.NewsService.Models.Dto.ModelResponse;
+
 using System.Threading.Tasks;
 
 namespace LT.DigitalOffice.NewsService.Mappers.ResponsesMappers.Interfaces
--- src/NewsService.Mappers/ResponsesMappers/NewsResponseMapper.cs
+++ src/NewsService.Mappers/ResponsesMappers/NewsResponseMapper.cs
@@ -8,6 +8,8 @@ using LT.DigitalOffice.NewsService.Models.Dto.ModelResponse;
 using LT.DigitalOffice.NewsService.Models.Dto.Models;
 using MassTransit;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Threading.Tasks;
 
 namespace LT.DigitalOffice.NewsService.Mappers.ResponsesMappers
@@ -15,10 +17,13 @@ namespace LT.DigitalOffice.NewsService.Mappers.ResponsesMappers
     public class NewsResponseMapper : INewsResponseMapper
     {
         private IRequestClient<IGetUserDataRequest> _client;
+        private readonly ILogger _logger;
         public NewsResponseMapper(
-            [FromServices] IRequestClient<IGetUserDataRequest> client)
+            [FromServices] IRequestClient<IGetUserDataRequest> client,
+            ILogger<NewsResponseMapper> logger)
         {
             _client = client;
+            _logger = logger;
         }
         public async Task<NewsResponse> Map(DbNews value)
         {
@@ -27,29 +32,33 @@ namespace LT.DigitalOffice.NewsService.Mappers.ResponsesMappers
                 throw new BadRequestException();
             }
 
-            var authorRequest = IGetUserDataRequest.CreateObj(value.AuthorId);
+            User author = new User();
+            User sender = new User();
 
-            var authorResponse = await _client.GetResponse<IOperationResult<IGetUserDataResponse>>(authorRequest);
+            try
+            {
+                var authorRequest = IGetUserDataRequest.CreateObj(value.AuthorId);
+                var authorResponse = await _client.GetResponse<IOperationResult<IGetUserDataResponse>>(authorRequest);
+                author.Id = authorResponse.Message.Body.Id;
+                author.FIO = $"{authorResponse.Message.Body.LastName} {authorResponse.Message.Body.FirstName} {authorResponse.Message.Body.MiddleName}".Trim();
 
-            var senderRequest = IGetUserDataRequest.CreateObj(value.SenderId);
-
-            var senderResponse = await _client.GetResponse<IOperationResult<IGetUserDataResponse>>(senderRequest);
+                var senderRequest = IGetUserDataRequest.CreateObj(value.SenderId);
+                var senderResponse = await _client.GetResponse<IOperationResult<IGetUserDataResponse>>(senderRequest);
+                sender.Id = senderResponse.Message.Body.Id;
+                sender.FIO = $"{senderResponse.Message.Body.LastName} {senderResponse.Message.Body.FirstName} {senderResponse.Message.Body.MiddleName}".Trim();
+            }
+            catch (Exception exception)
+            {
+                _logger.LogInformation(exception.Message);
+            }
 
             return new NewsResponse
             {
                 Id = value.Id,
                 Content = value.Content,
                 Subject = value.Subject,
-                Author = new User
-                {
-                    Id = authorResponse.Message.Body.Id,
-                    FIO = $"{authorResponse.Message.Body.LastName} {authorResponse.Message.Body.FirstName} {authorResponse.Message.Body.MiddleName}".Trim()
-                },
-                Sender = new User
-                {
-                    Id = senderResponse.Message.Body.Id,
-                    FIO = $"{senderResponse.Message.Body.LastName} {senderResponse.Message.Body.FirstName} {senderResponse.Message.Body.MiddleName}".Trim()
-                },
+                Author = author,
+                Sender = sender,
                 CreatedAt = value.CreatedAt
             };
         }
