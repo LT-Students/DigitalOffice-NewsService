241d433b73a0f746750329a05027858e1244f496
Mariya <63921256+MariyaSin@users.noreply.github.com>
2021-03-31 19:35:06 +0300

    Add news response model (#19)
    * added response model
    
    * updated model response
    
    * updated models
    
    * updated mapper and models
    
    * fixed mapper
    
    * rename GetUserInfo to GetUserData
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * rename config and appsetings from GetUserInfo to GetUserData
    
    * added logger to mapper
    
    * fix log error type
    
    * Remove await/async

---
+++ src/NewsService.Mappers/ResponsesMappers/Interfaces/INewsResponseMapper.cs
@@ -0,0 +1,15 @@
+﻿using LT.DigitalOffice.NewsService.Mappers.Interfaces;
+using LT.DigitalOffice.NewsService.Models.Db;
+using LT.DigitalOffice.NewsService.Models.Dto.ModelResponse;
+
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.NewsService.Mappers.ResponsesMappers.Interfaces
+{
+    /// <summary>
+    /// Represents mapper. Provides methods for converting an object of News value <see cref="DbNews"/>
+    /// </summary>
+    public interface INewsResponseMapper : IMapper<DbNews, NewsResponse>
+    {
+    }
+}
---
+++ src/NewsService/Configuration/RabbitMqConfig.cs
@@ -0,0 +1,9 @@
+﻿using LT.DigitalOffice.Kernel.Broker;
+
+namespace LT.DigitalOffice.NewsService.Configuration
+{
+    public class RabbitMqConfig : BaseRabbitMqOptions
+    {
+        public string GetUserDataEndpoint { get; set; }
+    }
+}
