fc88f2cc1e7b94e6e0030daa02f090d6f91a8f18
KostinP <st069165@student.spbu.ru>
2021-04-24 02:42:04 +0300

    Update version

--- src/NewsService/Startup.cs
+++ src/NewsService/Startup.cs
@@ -76,7 +76,7 @@ namespace LT.DigitalOffice.NewsService
                 .GetSection(BaseServiceInfoConfig.SectionName)
                 .Get<BaseServiceInfoConfig>();
 
-            Version = "1.2.3";
+            Version = "1.2.4";
             Description = "NewsService, is intended to work with the news - create them, update info and etc.";
             StartTime = DateTime.UtcNow;
             ApiName = $"LT Digital Office - {_serviceInfoConfig.Name}";
