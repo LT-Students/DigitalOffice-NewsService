ff4f1de869c17195dc4cf8e984d14a0e3473c2d7
Mariya <mariyasin@list.ru>
2022-01-12 17:09:18 +0300

    merge from develop

--- src/NewsService.Models.Dto/Configuration/RabbitMqConfig.cs
+++ src/NewsService.Models.Dto/Configuration/RabbitMqConfig.cs
@@ -4,7 +4,6 @@ using LT.DigitalOffice.Models.Broker.Common;
 using LT.DigitalOffice.Models.Broker.Requests.Admin;
 using LT.DigitalOffice.Models.Broker.Requests.Department;
 using LT.DigitalOffice.Models.Broker.Requests.Image;
-using LT.DigitalOffice.Models.Broker.Requests.News;
 using LT.DigitalOffice.Models.Broker.Requests.TextTemplate;
 using LT.DigitalOffice.Models.Broker.Requests.User;
 
@@ -12,13 +11,13 @@ namespace LT.DigitalOffice.NewsService.Models.Dto.Configuration
 {
   public class RabbitMqConfig : BaseRabbitMqConfig
   {
+    public string GetNewsDataEndpoint { get; set; }
+
     [AutoInjectRequest(typeof(ICreateServiceEndpointsRequest))]
     public string CreateServiceEndpointsEndpoint { get; set; }
 
     [AutoInjectRequest(typeof(ICreateKeywordsRequest))]
     public string CreateKeywordsEndpoint { get; set; }
-
-    public string GetNewsDataEndpoint { get; set; }
 
     [AutoInjectRequest(typeof(IGetImagesRequest))]
     public string GetImagesEndpoint { get; set; }
